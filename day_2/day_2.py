88132ae06a1b65f3712e7326d6ff29037fcf769c
Chai <chaifillerup@gmail.com>
2021-12-02 11:21:00 +0100

    Add files via upload

---
+++ day_2/day_2.py
@@ -0,0 +1,25 @@
+import numpy as np
+data = np.loadtxt("day_2_data.txt", dtype={'names':('col1', 'col2'), 'formats':('U10','i4')})
+conv = {'forward':0, 'down':1, 'up':-1}
+
+horizontal = 0
+depth = 0
+aim = 0
+
+def one(): 
+    horizontal = 0
+    depth = 0
+    for command in data:
+        horizontal += (1-abs(conv[command[0]]))*command[1]
+        depth += conv[command[0]]*command[1]
+    print(horizontal*depth)
+    
+def two():
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for command in data:
+        horizontal += (1-abs(conv[command[0]]))*command[1]
+        depth += (1-abs(conv[command[0]]))*command[1]*aim
+        aim += conv[command[0]]*command[1]
+    print(horizontal*depth)
