0ab2abe7435aed7b76da54d9777ec3ede8e53983
Chai <chaifillerup@gmail.com>
2021-12-02 11:21:30 +0100

    Update day_2.py

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,10 +2,6 @@ import numpy as np
 data = np.loadtxt("day_2_data.txt", dtype={'names':('col1', 'col2'), 'formats':('U10','i4')})
 conv = {'forward':0, 'down':1, 'up':-1}
 
-horizontal = 0
-depth = 0
-aim = 0
-
 def one(): 
     horizontal = 0
     depth = 0
